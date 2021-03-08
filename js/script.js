b7d265fd185b23316acb9e6450588b41784f75e5
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-08 15:32:17 -0500

    question appears on web page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('connected...')
+
+let xhr = new XMLHttpRequest()
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let jsonData = JSON.parse(this.responseText);
+        document.getElementById('demo').innerHTML += jsonData[0].question;
+    }
+};
+
+xhr.open('GET', 'http://jservice.io/api/random', true);
+xhr.send();
+
+
