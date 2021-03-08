3fab47e9105dc8fc2d8abc17a5b8a30ef3e8636d
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-08 16:05:07 -0500

    update

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ let xhr = new XMLHttpRequest()
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let jsonData = JSON.parse(this.responseText);
-        document.getElementById('demo').innerHTML += jsonData[0].question;
+        question(jsonData[0].question);
+        answer(jsonData[0].answer);
     }
 };
 
 xhr.open('GET', 'http://jservice.io/api/random', true);
 xhr.send();
 
+
+function question(categories){
+    document.getElementById('demo').innerHTML = categories
+};
+
+function answer(categories){
+    document.getElementById('answer').innerHTML = categories
+}
 
