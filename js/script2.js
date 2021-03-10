7b8585fa0c1f6c9894ec6cc06523e0af06a32c09
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-10 16:32:38 -0500

    update

--- js/script2.js
+++ js/script2.js
@@ -1,24 +1,64 @@
-function callOnCategoryData() {
-    document.getElementById('category').style.display = 'none'
-    let xhrttp = new XMLHttpRequest()
 
-    xhrttp.onreadystatechange = function() {
+
+let url = 'http://jservice.io/api/categories?count=5'
+
+let xhrttp = new XMLHttpRequest()
+
+xhrttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let jsonData = JSON.parse(this.responseText);
+        createButton(jsonData)
+    }
+};
+
+xhrttp.open('GET', url, true);
+xhrttp.send();
+
+let createButton = (arr) => {
+    let container = document.getElementById('container');
+    for (let object of arr) {
+        let createdButton = document.createElement('button')
+        createdButton.setAttribute('categoryid', object.id)
+        createdButton.innerText = object.title
+        createdButton.addEventListener('click', loadCategories)
+        container.insertBefore(createdButton, document.getElementById('demo'));
+    }
+};
+
+function loadCategories(evt) {
+    document.getElementById('answer').style.display = 'none'
+    // console.log(evt.target.getAttribute('categoryid'));
+    let url = 'http://jservice.io/api/clues?category=' + evt.target.getAttribute('categoryid')
+
+    let xhr = new XMLHttpRequest()
+
+    xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            let jsonData = JSON.parse(this.responseText);
-            chooseCategory(jsonData[0].title )
+            let jsonData = JSON.parse(this.responseText)
+                question(jsonData[0].question)
+                answer(jsonData[0].answer)
         }
-    };
+    }
 
-    xhrttp.open('GET', 'http://jservice.io/api/categories', true);
-    xhrttp.send();
-}
-
-let chooseCategory = function category(data) {
-    document.getElementById('category').innerHTML = data
+    xhr.open('GET', url, true);
+    xhr.send();
 } 
 
-function showCategory(evt) {
-    document.getElementById('category').style.display = 'block'
+function question(data) {
+    document.getElementById('demo').innerHTML = data
 }
 
-callOnCategoryData();
+function answer(data){
+    document.getElementById('answer').innerHTML = data
+}
+
+function showAnswer(evt) {
+    document.getElementById('answer').style.display = 'block' 
+    console.log(evt);
+}
+
+const refreshButton = document.getElementById('nextQuest');
+refreshButton.addEventListener('click', loadCategories)
+
+
+
